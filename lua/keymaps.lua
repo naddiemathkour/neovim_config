d6a5d1a9cbb75e0ac1d7062d47094c11c0a53be1
naddiemathkour <naddiemathkour@gmail.com>
2024-10-14 10:51:59 -0400

    added movement commands

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,3 +3,7 @@ vim.api.nvim_set_keymap('n', '<c-t>', ':Neotree toggle<CR>', { noremap = true, s
 
 -- Telescope
 vim.api.nvim_set_keymap('n', '<c-p>', ':Telescope find_files<CR>', { noremap = true, silent = true})
+
+-- Movement
+vim.api.nvim_set_keymap('n', '<C-j>', ':JumpLines 10<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-k>', ':JumpUp 10<CR>', { noremap = true, silent = true})
